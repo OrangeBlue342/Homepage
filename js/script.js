de398de3b3bc37542c00ccd750424c3dac5389ef
Joanna Czub <joannaczub53@gmail.com>
2022-11-23 10:12:10 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Cześć!")
+let przycisk = document.querySelector(".przycisk")
+let naglowek = document.querySelector(".naglowek")
+let body = document.querySelector(".background")
+let header = document.querySelector (".header")
+
+przycisk.addEventListener("click", () => {
+    header.remove();
+})
+
+let button = document.querySelector(".button")
+
+button.addEventListener("click", () => {
+    if(button.innerText === "Włącz tryb ciemny") {
+        button.innerText = "Wyłącz tryb ciemny";
+   body.classList.toggle("darkbackground"); }
+});
+
+
+
