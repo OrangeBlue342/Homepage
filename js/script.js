986578e32d03591a1c46c204a0b1b3ba41914d1f
Joanna Czub <joannaczub53@gmail.com>
2022-11-25 14:35:33 +0100

    functions added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
+{
 function welcome() {
     console.log("Cześć!")
 }
 
 welcome;
 
-let przycisk = document.querySelector(".przycisk")
-let naglowek = document.querySelector(".naglowek")
-let body = document.querySelector(".background")
-let header = document.querySelector (".header")
+const przycisk = document.querySelector(".przycisk")
+const header = document.querySelector (".header")
+
+const onChangeBackgroundClick = () => {
+    const body = document.querySelector(".background");
+    const button = document.querySelector(".button")
+
+    button.addEventListener("click", () => {
+        if(button.innerText === "Włącz tryb ciemny") {
+            button.innerText = "Wyłącz tryb ciemny";
+        }});
+
+    body.classList.toggle("darkbackground");
+}
+
+onChangeBackgroundClick();
 
 przycisk.addEventListener("click", () => {
     header.remove();
 })
 
-let button = document.querySelector(".button")
-
-button.addEventListener("click", () => {
-    if(button.innerText === "Włącz tryb ciemny") {
-        button.innerText = "Wyłącz tryb ciemny";
-   body.classList.toggle("darkbackground"); }
-});
-
-
-
+}
